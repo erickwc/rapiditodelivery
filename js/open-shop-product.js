5175e8bf2add3d572770c927fcfad230b0be3bc1
erickwc <erickcaceres1717@gmail.com>
2023-12-28 12:42:00 -0600

    Added send whatsapp section

--- js/open-shop-product.js
+++ js/open-shop-product.js
@@ -1,35 +1,3 @@
-/*btn-amounts*/
-
-// const productContainers = document.querySelectorAll('.product-card');
-
-// productContainers.forEach((container) => {
-//     const addBtn = container.querySelector('.add');
-//     const removeBtn = container.querySelector('.remove');
-//     const numberProductHeader = container.querySelector('.name-product .NumberProduct');
-//     const numberProductSection = container.querySelector('.amount-product .NumberProduct');
-
-//     let total = 0;
-
-//     function updateNumberProduct() {
-//         numberProductHeader.textContent = total;
-//         numberProductSection.textContent = total;
-//     }
-
-//     function Add() {
-//         total += 1;
-//         updateNumberProduct();
-//     }
-
-//     function Remove() {
-//         if (total > 0) {
-//             total -= 1;
-//             updateNumberProduct();
-//         }
-//     }
-
-//     addBtn.addEventListener("click", Add);
-//     removeBtn.addEventListener("click", Remove);
-// });
 
 /*btn-actions*/
 
@@ -64,16 +32,26 @@ function assignEventsBtnActions() {
 
 /**/
 
+const productos = [];
+let productIdCounter = 1;
 
 const btnAddProduct = document.querySelector('.btn-add-product');
 const productsContainer = document.querySelector('.products-added');
+let allProductsInfo = "";
+
+function updateAllProductsInfo() {
+    allProductsInfo = productos.map(producto => `Producto - Cantidad(${producto.productoCantidad})\n ${producto.productoDescripcion}\n\n`).join('');
+}
 
 function AddedProduct() {
-
     const descProduct = document.querySelector('.descP').value;
     const numberProduct = document.querySelector('.numberP').value;
+    const inputContainerAddedProduct = document.querySelector('.input-container-added-product');
 
-    if (descProduct.trim() !== "" && numberProduct.trim() !== "") {
+    if (descProduct.trim() !== "" && numberProduct.trim() !== "")  {
+
+        const productId = `${productIdCounter++}`;
+
         let cardProduct = document.createElement('article');
         cardProduct.classList.add('product-card');
 
@@ -101,10 +79,9 @@ function AddedProduct() {
         sectionAmountBtn.appendChild(btnRemove);
 
         let numberProductAdd = document.createElement('p');
-        numberProductAdd.classList.add('NumberProduct', 'original'); // Asigna una clase específica, por ejemplo, 'original'
+        numberProductAdd.classList.add('NumberProduct', 'original');
         numberProductAdd.textContent = numberProduct;
         sectionAmountBtn.appendChild(numberProductAdd);
-
 
         let btnAdd = document.createElement('button');
         btnAdd.classList.add('add', 'btn-amount');
@@ -136,15 +113,29 @@ function AddedProduct() {
 
         productsContainer.appendChild(cardProduct);
 
+        productos.push({
+            productId: productId,
+            productoCantidad: numberProduct,
+            productoDescripcion: descProduct
+        });
+        updateAllProductsInfo();
+        
+
+        // Llama a la función fuera de AddedProduct después de haber agregado un producto
         assignEventsBtnActions();
         assignEventsBtnAmount();
-    }
-    else {
-        alert("Agrega datos bruto");
+
+        inputContainerAddedProduct.value = allProductsInfo;
+    } else {
+        alert("Agrega datos válidos");
     }
 }
 
-btnAddProduct.addEventListener('click', AddedProduct)
+
+btnAddProduct.addEventListener('click', AddedProduct);
+console.log(productos);
+
+
 
 function assignEventsBtnAmount() {
     const productContainers = document.querySelectorAll('.product-card');
@@ -182,47 +173,40 @@ function assignEventsBtnAmount() {
             removeBtn.addEventListener("click", Remove);
         }
     });
+
+}
+
+
+function isMobile() {
+    if (sessionStorage.desktop)
+        return false;
+    else if (localStorage.mobile)
+        return true;
+    var mobile = ['iphone', 'ipad', 'android', 'blackberry', 'nokia', 'opera mini', 'windows mobile', 'windows phone', 'iemobile'];
+    for (var i in mobile)
+        if (navigator.userAgent.toLowerCase().indexOf(mobile[i].toLowerCase()) > 0) return true;
+    return false;
 }
 
 
 
+const formulario = document.querySelector('#formulario');
+const buttonSubmit = document.querySelector('#submit');
+const urlDesktop = 'https://web.whatsapp.com/';
+const urlMobile = 'whatsapp://';
+const telefono = '50377774562';
 
-
-
-// function assignEventsBtnAmount() {
-//     const productContainers = document.querySelectorAll('.product-card');
-
-//     productContainers.forEach((container) => {
-//         const addBtn = container.querySelector('.add');
-//         const removeBtn = container.querySelector('.remove');
-//         const numberProductAdd = container.querySelector('.amount-product .NumberProduct');
-//         const numberProductTitle = container.querySelector('.grid .NumberProduct');
-
-//         // Obtén el valor inicial del elemento NumberProduct
-//         let numberProduct = parseInt(numberProductAdd.textContent) || 0;
-
-//         function updateNumberProduct() {
-//             if (numberProductAdd && numberProductTitle) {
-//                 numberProductAdd.textContent = numberProduct;
-//                 numberProductTitle.textContent = numberProduct;
-//             }
-//         }
-
-//         function Add() {
-//             numberProduct++;
-//             updateNumberProduct();
-//         }
-
-//         function Remove() {
-//             if (numberProduct > 0) {
-//                 numberProduct--;
-//                 updateNumberProduct();
-//             }
-//         }
-
-//         if (addBtn && removeBtn) {
-//             addBtn.addEventListener("click", Add);
-//             removeBtn.addEventListener("click", Remove);
-//         }
-//     });
-// }
+formulario.addEventListener('submit', (event) => {
+    event.preventDefault()
+    buttonSubmit.disabled = true
+    setTimeout(() => {
+        let productos = document.querySelector('.input-container-added-product').value
+        let mensaje = 'send?phone=' + telefono + '&text=*_Formulario Easy App CODE_*%0A*¿Cual es tu nombre?*%0A' +  productos + '%0A*¿Cuáles son tus apellidos?*%0A' + productos + '%0A*¿Cuál es tu correo electrónico?*%0A' + productos + ''
+        if(isMobile()) {
+            window.open(urlMobile + mensaje, '_blank')
+        }else{
+            window.open(urlDesktop + mensaje, '_blank')
+        }
+        buttonSubmit.disabled = false
+    }, 3000);
+});
